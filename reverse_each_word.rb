def23f658425d92d63e2cf93fe771f9cb8796923
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-02 22:28:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word (phrase)
   #words = phrase.split(" ")
   #newPhrase = words.collect {|w| w.reverse}
   #newPhrase.join(" ")
-  newPhrase = words.collect {|w| w.reverse}
+  newPhrase = phrase.collect {|w| w.reverse}
 end 
