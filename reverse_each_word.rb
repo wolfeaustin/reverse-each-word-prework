b9f7d44bbcdc1d826255d67e45c0f0d71000b67e
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-02 22:28:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word (phrase)
-  #words = phrase.split(" ")
-  #newPhrase = words.collect {|w| w.reverse}
-  #newPhrase.join(" ")
-  newPhrase = phrase.collect {|w| w.reverse}
+  words = phrase.split(" ")
+  newPhrase = words.collect {|w| w.reverse}
+  newPhrase.join(" ")
 end 
